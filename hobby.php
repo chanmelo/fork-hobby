88e6ce2b79e7f3968bf03ad7959b46ba4ed19d65
ciitra <159239257+ciitra@users.noreply.github.com>
2024-03-06 15:23:11 +0700

    Update hobby.php
    add hobby

--- hobby.php
+++ hobby.php
@@ -1,5 +1,10 @@
 <html>
 <head>
-  <title></title>
+  <title>Hobby</title>
 </head>
+  <body>
+    <?php
+    echo "Hobby: Badminton, Traveling";
+    echo "tergantung mood juga sii";
+  </body>
 </html>
